3347a68d8f6393bc1d7b04bdaa323a72e6b74d6c
emacip <emacipbelmonte@gmail.com>
2015-08-21 12:09:21 +0100

    change the routes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'qualification/index'
+  root 'qualification#index'
 
+  get 'qualification/index' => 'qualification#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
